88b4acfb5d1df6efaccf827b4d94da3518c6f67f
Maksim Pecherskiy <max@maksimize.com>
2015-06-30 09:37:10 -0700

    Updating builder to new version.

--- getBookPDF.js
+++ getBookPDF.js
@@ -1,4 +1,4 @@
 require('shelljs/global');
 
 rm('-rf', '~/Dropbox/Public/SDShare/council_report_show.pdf');
-exec('gitbook pdf . ~/Dropbox/Public/SDShare/council_report_show.pdf');
+exec('gitbook pdf . ~/Dropbox/Public/SDShare/council_report_show.pdf --gitbook=2.1.0-mrm');
